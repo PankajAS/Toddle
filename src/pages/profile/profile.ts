f7b8ba1a001fe380ac426a3d24e0032264a61301
Plus 3 <pankajs@plusonesoftwares.com>
2017-05-31 10:20:52 +0530

    profile design

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProfilePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+  name:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.name = this.navParams.data;
+    console.log('ionViewDidLoad ProfilePage');
+  }
+
+}
