4b0f5c23c358db20ea7798209b44177248c3bcc1
Plus 3 <pankajs@plusonesoftwares.com>
2017-06-13 19:33:27 +0530

    location service

---
+++ src/providers/location-service/location-service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Headers } from '@angular/http';
+
+/*
+  Generated class for the LocationServiceProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class LocationServiceProvider {
+  url = 'http://kdv-api-acceptatie.kdv1.tactics.be/user/';
+
+
+  constructor(public http: Http) {
+  }
+
+  getLocation(userId:string,token:string){
+    return new Promise(resolve => {
+      var header = new Headers();
+      header.append('Content-Type', 'application/json');
+      this.http.get(this.url+userId + '/token/' + token + '/locations', {headers: header})
+        .map(res => res.json())
+        .subscribe(data => {
+            resolve(data);
+          },
+          error => console.log(error),
+          () => console.log("Finished")
+        );
+    });
+  }
+
+}
